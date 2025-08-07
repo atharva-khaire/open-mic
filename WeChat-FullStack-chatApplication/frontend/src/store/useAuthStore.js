4e404143b3760fb0f8c99dac8bd096dc1f6c080b
atharva-khaire <atharvakhaire2004@gmail.com>
2025-08-08 01:25:17 +0530

    commites

--- WeChat-FullStack-chatApplication/frontend/src/store/useAuthStore.js
+++ WeChat-FullStack-chatApplication/frontend/src/store/useAuthStore.js
@@ -8,7 +8,7 @@ import { io } from "socket.io-client";
 
 const BASE_URL = import.meta.env.MODE === "development"
   ? "http://localhost:5001"
-  : "https://wechat-backend.onrender.com"; // Replace with your actual backend render URL
+  : "https://wechat-fullstack-chatapplication.onrender.com"; // Replace with your actual backend render URL
 
 
 
