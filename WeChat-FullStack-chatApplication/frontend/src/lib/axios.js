34bc8d2e49ca89e31a9a0264293fbb4c248f1210
atharva-khaire <atharvakhaire2004@gmail.com>
2025-08-07 23:09:03 +0530

    commited

--- WeChat-FullStack-chatApplication/frontend/src/lib/axios.js
+++ WeChat-FullStack-chatApplication/frontend/src/lib/axios.js
@@ -6,11 +6,19 @@
 // });
 
 
+// import axios from "axios";
+
+// // Use VITE_API_URL for production and localhost for development
+// const baseURL =
+//   import.meta.env.MODE === `${import.meta.env.VITE_API_URL}/api`;
+
+// export const axiosInstance = axios.create({
+//   baseURL,
+//   withCredentials: true,
+// });
 import axios from "axios";
 
-// Use VITE_API_URL for production and localhost for development
-const baseURL =
-  import.meta.env.MODE === `${import.meta.env.VITE_API_URL}/api`;
+const baseURL = `${import.meta.env.VITE_API_URL}/api`; // ✅ correct
 
 export const axiosInstance = axios.create({
   baseURL,
