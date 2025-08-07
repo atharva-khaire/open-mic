d0cec1c3f7cd033e73ea8e661076a85860879447
atharva-khaire <atharvakhaire2004@gmail.com>
2025-08-07 22:05:05 +0530

    updated  frontend url

--- WeChat-FullStack-chatApplication/backend/src/lib/socket.js
+++ WeChat-FullStack-chatApplication/backend/src/lib/socket.js
@@ -5,9 +5,11 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
+const frontendURL = process.env.FRONTEND_URL || "http://localhost:5173";
+
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [frontendURL],
   },
 });
 
